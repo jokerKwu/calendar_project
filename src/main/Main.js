3736c1ab33831d68488b4d5132f79d06354a39d3
jokerKwu <pkjhj485@naver.com>
2021-03-22 15:26:21 +0900

    메인 이미지 슬라이드 적용

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,22 +1,104 @@
-  import {StatusBar} from 'expo-status-bar';
-import React from 'react';
-import {StyleSheet, Text, View,Image} from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import React, {Component} from 'react';
+import {  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight} from 'react-native';
+import ImageSlider from 'react-native-image-slider';
 
- function Main() {
-    return (  
-      <View style={{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
-        
-      }}>
-        <Image style={{
-          width: 400,
-          height: 500,
-        }}
-        source={{uri:"https://images.unsplash.com/photo-1478265409131-1f65c88f965c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8c25vd3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"}}
+export default class Main extends Component {
+  render() {
+    const images = [
+      'https://placeimg.com/640/640/nature',
+      'https://placeimg.com/640/640/people',
+      'https://placeimg.com/640/640/animals',
+      'https://placeimg.com/640/640/beer',
+    ];
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.content1}>
+          <Text style={styles.contentText}>Content 1</Text>
+        </View>
+        <ImageSlider
+          loopBothSides
+          autoPlayWithInterval={10000}
+          images={images}
+          customSlide={({ index, item, style, width }) => (
+            // It's important to put style here because it's got offset inside
+            <View key={index} style={[style, styles.customSlide]}>
+              <Image source={{ uri: item }} style={styles.customImage} />
+            </View>
+          )}
+          customButtons={(position, move) => (
+            <View style={styles.buttons}>
+              {images.map((image, index) => {
+                return (
+                  <TouchableHighlight
+                    key={index}
+                    underlayColor="#ddd"
+                    onPress={() => move(index)}
+                    style={styles.button}
+                  >
+                    <Text style={position === index && styles.buttonSelected}>
+                      {index + 1}
+                    </Text>
+                  </TouchableHighlight>
+                );
+              })}
+            </View>
+          )}
         />
       </View>
     );
+  }
 }
-export default Main;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  slider: { backgroundColor: '#000', height: 350 },
+  content1: {
+    width: '100%',
+    height: 50,
+    marginBottom: 10,
+    backgroundColor: '#ccc',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentText: { color: '#fff' },
+  buttons: {
+    zIndex: 1,
+    height: 15,
+    marginTop: -25,
+    marginBottom: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 3,
+    width: 15,
+    height: 15,
+    opacity: 0.9,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonSelected: {
+    opacity: 1,
+    color: 'red',
+  },
+  customSlide: {
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  customImage: {
+    width: '100%',
+    height: 600,
+  },
+});
+
